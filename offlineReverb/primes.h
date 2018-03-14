d167279fbdbe282f8d3fbfe4bf14e7200de355e2
Unknown <sirhans@gmail.com>
2018-03-14 15:12:07 +0700

    add list of primes in primes.h

---
+++ offlineReverb/primes.h
@@ -0,0 +1,13 @@
+//
+//  primes.h
+//  offlineReverb
+//
+//  Created by hans anderson on 3/14/18.
+//  Copyright © 2018 Hans. All rights reserved.
+//
+
+#ifndef primes_h
+#define primes_h
+
+
+#endif /* primes_h */
